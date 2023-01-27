73c2536f04774ff04e0946537c592a464f2aec2c
Uzui Tengen <121469941+Uzui01@users.noreply.github.com>
2023-01-27 12:53:22 +0800

    BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/corvus_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    corvus_lancelot-user \
+    corvus_lancelot-userdebug \
+    corvus_lancelot-eng
